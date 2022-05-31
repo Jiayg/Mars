53b208620ddf9f1ca1e9ccdea6d8e3bcebb03b5c
Jiayg <32000884258@qq.com>
2022-05-31 12:42:36 +0800

    feat:ids用户管理

---
+++ modules/AccountManagement/Volo.Abp.Account.Web/AbpAccountWebAutomapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Volo.Abp.Account.Dtos;
+using Volo.Abp.Account.Web.Pages.Account.Components.ProfileManagementGroup.PersonalInfo;
+
+namespace Volo.Abp.Account.Web;
+
+public class AbpAccountWebAutoMapperProfile : Profile
+{
+    public AbpAccountWebAutoMapperProfile()
+    {
+        CreateMap<ProfileDto, AccountProfilePersonalInfoManagementGroupViewComponent.PersonalInfoModel>();
+    }
+}
